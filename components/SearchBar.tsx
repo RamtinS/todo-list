8fc7148c3801e833a9c98b0f977f920066526eab
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-10-20 01:09:21 +0200

    feat(components): Add search bar component

---
+++ components/SearchBar.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import {SearchBarProps} from "@/models/Type";
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+    onSearch(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder={'Search'}
+        clearButtonMode={"always"}
+        autoCorrect={false}
+        style={styles.textInput}
+        value={searchText}
+        onChangeText={handleSearch}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 20,
+    position: 'absolute',
+    top: 0,
+    zIndex: 1,
+  },
+  textInput: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderColor: "#090909",
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    fontSize: 18
+  }
+})
