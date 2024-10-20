a6baff21e6d9930b3b92ffbe0af47ef6ed5b5e40
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-10-20 22:30:46 +0200

    refactor(models): Change TodoList to be a type

--- models/TodoList.ts
+++ models/TodoList.ts
@@ -1,49 +1,41 @@
+export type TodoList = {
+  title: string;
+  completedItems: string[];
+  nonCompletedItems: string[];
+};
 
-export class TodoList {
-  private readonly _title: string;
-  private _completedItems: string[];
-  private _nonCompletedItems: string[];
 
-  constructor(title: string) {
-    this._title = title;
-    this._completedItems = []
-    this._nonCompletedItems = []
+export const addItem = (todoList: TodoList, item: string): TodoList => ({
+  ...todoList,
+  nonCompletedItems: [...todoList.nonCompletedItems, item],
+});
+
+// Function to move an item from non-completed to completed
+export const moveToCompleted = (todoList: TodoList, index: number): TodoList => {
+  if (index < 0 || index >= todoList.nonCompletedItems.length) {
+    throw new RangeError("Index of item is out of range");
   }
 
-  public get title(): string{
-    return this._title;
+  const item = todoList.nonCompletedItems[index];
+  return {
+    ...todoList,
+    nonCompletedItems: todoList.nonCompletedItems.filter((_, i) => i !== index),
+    completedItems: [...todoList.completedItems, item],
+  };
+};
+
+// Function to move an item from completed to non-completed
+export const moveToNonCompleted = (todoList: TodoList, index: number): TodoList => {
+  if (index < 0 || index >= todoList.completedItems.length) {
+    throw new RangeError("Index of item is out of range");
   }
 
-  public get completedItems(): string[] {
-    return this._completedItems;
-  }
+  const item = todoList.completedItems[index];
+  return {
+    ...todoList,
+    completedItems: todoList.completedItems.filter((_, i) => i !== index),
+    nonCompletedItems: [...todoList.nonCompletedItems, item],
+  };
+};
 
-  public get  nonCompletedItems(): string[] {
-    return this._nonCompletedItems;
-  }
-
-  public addItem(item: string): void {
-    this._nonCompletedItems.push(item);
-  }
-
-  public moveToCompleted(index: number): void {
-    if (index < 0 || index >= this._nonCompletedItems.length) {
-      throw new RangeError("Index of item is out of range");
-    }
-
-    const item: string = this._nonCompletedItems[index];
-    this._nonCompletedItems.splice(index, 1);
-    this._completedItems.push(item);
-  }
-
-  public moveToNonCompleted(index: number): void {
-    if (index < 0 || index > this._completedItems.length) {
-      throw new RangeError("Index of item is out of range");
-    }
-
-    const item: string = this._completedItems[index];
-    this._completedItems.splice(index, 1);
-    this._nonCompletedItems.push(item);
-  }
-}
 
