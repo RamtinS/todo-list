c41e0b8c6e06321da5680ffad75bc3b45de8efdf
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-10-24 10:39:57 +0200

    refactor(models): Remove unused functions from TodoList

--- models/TodoList.ts
+++ models/TodoList.ts
@@ -1,41 +1,7 @@
 export type TodoList = {
   title: string;
-  completedItems: string[];
-  nonCompletedItems: string[];
-};
-
-
-export const addItem = (todoList: TodoList, item: string): TodoList => ({
-  ...todoList,
-  nonCompletedItems: [...todoList.nonCompletedItems, item],
-});
-
-// Function to move an item from non-completed to completed
-export const moveToCompleted = (todoList: TodoList, index: number): TodoList => {
-  if (index < 0 || index >= todoList.nonCompletedItems.length) {
-    throw new RangeError("Index of item is out of range");
-  }
-
-  const item = todoList.nonCompletedItems[index];
-  return {
-    ...todoList,
-    nonCompletedItems: todoList.nonCompletedItems.filter((_, i) => i !== index),
-    completedItems: [...todoList.completedItems, item],
-  };
-};
-
-// Function to move an item from completed to non-completed
-export const moveToNonCompleted = (todoList: TodoList, index: number): TodoList => {
-  if (index < 0 || index >= todoList.completedItems.length) {
-    throw new RangeError("Index of item is out of range");
-  }
-
-  const item = todoList.completedItems[index];
-  return {
-    ...todoList,
-    completedItems: todoList.completedItems.filter((_, i) => i !== index),
-    nonCompletedItems: [...todoList.nonCompletedItems, item],
-  };
+  completedTasks: string[];
+  nonCompletedTasks: string[];
 };
 
 
