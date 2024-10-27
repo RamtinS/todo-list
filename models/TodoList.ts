7574a55f1e39931abbf85cf3c247c7f3a2738d1b
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-10-27 07:50:12 +0100

    feat(models): Add id TodoList and new type for holding metadata

--- models/TodoList.ts
+++ models/TodoList.ts
@@ -1,7 +1,11 @@
 export type TodoList = {
+  id: number
   title: string;
   completedTasks: string[];
   nonCompletedTasks: string[];
 };
 
-
+export type TodoListMeta = {
+  id: number;
+  title: string;
+};
