6f28e6c70d13fcae17dd499083116dc5f9cad165
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2024-10-27 08:30:17 +0100

    feat(app): Initialize directory and test data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,23 @@
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
 import {StatusBar} from "react-native";
+import {initializeDirectoryAndIndex, initializeTestData} from "@/utils/FileManager";
+import {useEffect} from "react";
 
 // Main entry point for the application.
 export default function RootLayout() {
+
+  // Create file directory and test data when starting app.
+  useEffect((): void => {
+    initializeDirectoryAndIndex().catch((error) => {
+      console.error("Failed to create data directory:", error);
+    });
+
+    initializeTestData(true).catch((error) => {
+      console.error("Failed to create test data:", error);
+    });
+
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content"/>
